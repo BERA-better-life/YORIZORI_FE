df0f81a4c6ab55e8b3b56a20189c3fcd0961b942
as-zini <aszini@naver.com>
2025-05-01 17:13:48 +0900

    #5 Design:회원가입 페이지 UI 완성

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -3,6 +3,7 @@ import BottomTabNavigation from "./BottomTabNavigation";
 import AddFreezerEl from "../ui/screen/AddFreezerEl";
 import PickExpDate from "../ui/screen/PickExpDate";
 import Login from "../ui/screen/Login";
+import Signup from "../ui/screen/Signup";
 
 
 const Stack = createStackNavigator();
@@ -19,6 +20,7 @@ const StackNavigation = () => {
       <Stack.Screen name="AddFreezerEl" component={AddFreezerEl}/>
       <Stack.Screen name="PickExpDate" component={PickExpDate}/>
       <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Signup" component={Signup}/>
     </Stack.Navigator>
   )
 }
--- src/ui/screen/Signup.js
+++ src/ui/screen/Signup.js
@@ -8,42 +8,43 @@ import MarginVertical from "../components/MarginVertical";
 import { useNavigation } from "@react-navigation/native";
 
 
-const Login = () => {
+const Signup = () => {
   const navigation = useNavigation();
+  const placeholderArray = ["이메일을 입력해주세요","영문, 숫자 포함 8~16글자","비밀번호 확인","닉네임을 입력해주세요"]
 
   return (
     <SafeAreaView style={{backgroundColor:colors.bgColor}}>
       <Body>
         <Image source={logo} style={{width:"45%", resizeMode:'contain'}}/>
-        <Title>로그인</Title>
+        <Title>회원가입</Title>
         <MarginVertical margin={20}/>
         <InputArea>
-          <View style={{width:'70%', justifyContent:'center', alignItems:'center'}}>
-            <InputBox placeholder="이메일을 입력해주세요"/>
-            <ValidCircle/>
-          </View>
-          <View style={{width:'70%', justifyContent:'center', alignItems:'center'}}>
-            <InputBox placeholder="비밀번호를 입력해주세요"/>
-            <ValidCircle/>
-          </View>
+          {placeholderArray.map((el,index) => {
+            return(
+            <View style={{width:'70%', justifyContent:'center', alignItems:'center'}} key={index}>
+              <InputBox placeholder={el}/>
+              <ValidCircle/>
+            </View>
+            )
+          })}
         </InputArea>
         <MarginVertical margin={15}/>
         <View style={{flexDirection:'row', width:"70%"}}>
-          <Text style={{color:colors.lightGray}}>아직 회원이 아니신가요? </Text>
-          <GoToSignUp onPress={() => navigation.navigate("Signup")}>
-            <GoToSignUpText>회원가입 하러가기</GoToSignUpText>
+          <Text style={{color:colors.lightGray}}>이미 회원이신가요? </Text>
+          <GoToSignUp onPress={() => navigation.navigate("Login")}>
+            <GoToSignUpText>로그인 하러가기</GoToSignUpText>
           </GoToSignUp>
         </View>
         <MarginVertical margin={55}/>
         <View style={{width:'100%', justifyContent:'center', alignItems:'center'}}>
-          <Button text={"로그인하기"} width={"70%"} handleButton={() => navigation.navigate("Tabs")}/>
+          <Button text={"회원가입하기"} width={"70%"} handleButton={() => navigation.navigate("Tabs")}/>
         </View>
       </Body>
     </SafeAreaView>
   )
 }
 
-export default Login
+export default Signup
 
 const Body = styled.View`
   width:${size.width}px;
